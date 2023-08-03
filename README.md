ea07cd51dee8c1eef76ac6b4866ea70671b4ed42
thesarfo <ernestsarfo100@gmail.com>
2023-08-03 09:15:11 +0000

    init files

---
+++ README.md
@@ -0,0 +1,46 @@
+# YT-DLD
+
+## Getting started
+
+### [View screenshot](https://github.com/thesarfo/yt-dld#screenshot)
+
+- **Installation**
+
+```shell
+$ git clone https://github.com/thesarfo/yt-dld.git
+```
+
+- **Change directory**
+
+```shell
+$ cd yt-dld
+```
+
+- **Virtual Environment** `Optional`
+
+```shell
+$ python venv venv
+$ source venv/bin/activate
+```
+
+- **Install dependencies**
+
+```shell
+$ pip install -r requirements.txt
+```
+
+- **Run the Bot**
+
+```shell
+$ python main.py
+```
+
+## Built With
+
+- [Python](https://www.python.org/) - The python is famous programming language
+- [Flask](https://github.com/pallets/flask) - The Python micro framework for building web applications.
+- [yt-dlp](https://github.com/yt-dlp/yt-dlp) - A youtube-dl fork with additional features and fixes
+
+
+## Screenshot
+![screenshot yt-dld](https://github.com/thesarfo/yt-dld/blob/main/screenshot/yt-dld.png?raw=true)
---
+++ main.py
@@ -0,0 +1,26 @@
+import re
+import os
+from flask import Flask, render_template, request
+from utils import REGEX, get_youtube_extract
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/', methods=['GET', 'POST'])
+def _index():
+    if request.method == 'POST':
+        youtube_url = request.form.get('youtube_url')
+        if re.match(REGEX, youtube_url):
+            user_dict = get_youtube_extract(youtube_url)
+            audio = user_dict['audio_formats']
+            return render_template('result.html',
+                                   audio_dict=audio,
+                                   thumbnail=user_dict['thumbnail'],
+                                   video_dict=user_dict['video_formats'],
+                                   video_title=user_dict['title']
+                                   )
+
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=os.environ.get("PORT", 5000), use_reloader=True, threaded=True)
